614ebb6863147bbe933b9e6d87cb1ea186d8f17d
Ramiro <ramiro.sena.eng@gmail.com>
2022-12-01 20:29:19 -0300

    check for at least one odd number

---
+++ fundamentals/section-4-indroduction-to-javascript/day-1-first-steps/check_for_odd.js
@@ -0,0 +1,16 @@
+// check for at least one odd number
+
+const c1 = 5;
+const c2 = 9;
+const c3 = 2;
+
+function check_for_odd() {
+
+  if (c1 % 2 == 0 || c2 % 2 == 0 || c3 % 2 == 0) {
+    return true;
+  }
+  return false;
+
+}
+
+console.log(check_for_odd())
