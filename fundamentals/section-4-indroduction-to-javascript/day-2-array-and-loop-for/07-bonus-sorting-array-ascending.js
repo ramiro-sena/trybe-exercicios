a3a611b6e8f5ebb47277d3c8d6ada0883e709cba
Ramiro <ramiro.sena.eng@gmail.com>
2022-12-03 19:12:42 -0300

    bonus sort array ascending order

---
+++ fundamentals/section-4-indroduction-to-javascript/day-2-array-and-loop-for/07-bonus-sorting-array-ascending.js
@@ -0,0 +1,14 @@
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+for (let index = 1; index < array.length; index += 1) { //for each item in array
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) { //for each iterated item
+    if (array[index] < array[secondIndex]) { // if current item is less than previous
+      let position = array[index];        
+      array[index] = array[secondIndex];     // switch places
+      array[secondIndex] = position;
+    }
+  }
+}
+
+console.log(array)
