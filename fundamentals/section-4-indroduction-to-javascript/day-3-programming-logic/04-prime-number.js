08b8aa650f6131eeccfd48da4c8cb58a4373279e
Ramiro <ramiro.sena.eng@gmail.com>
2022-12-04 00:00:56 -0300

    greatest prime number between 2 and 50

--- fundamentals/section-4-indroduction-to-javascript/day-3-programming-logic/04-prime-number.js
+++ fundamentals/section-4-indroduction-to-javascript/day-3-programming-logic/04-prime-number.js
@@ -1,13 +1,15 @@
 let primeNumbers = [];
 
 
-
 for (let i = 2; i <= 50; i++){
   let isPrime = true;
   for (let j = i; j >=1; j--){
     if (j != 1 && j != i && i % j === 0) isPrime = false;
   }
   if (isPrime) primeNumbers.push(i);
+
 }
 
-console.log(primeNumbers);
+greatestPrimeNumber = primeNumbers[primeNumbers.length - 1]
+
+console.log(greatestPrimeNumber);
