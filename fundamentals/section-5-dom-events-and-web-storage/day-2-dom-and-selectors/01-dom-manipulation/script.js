64a2460d59cced9466233d159dfef99f4cdc7d9a
Ramiro <ramiro.sena.eng@gmail.com>
2022-12-07 16:14:28 -0300

    2nd requirement accomplished - change color

--- fundamentals/section-5-dom-events-and-web-storage/day-2-dom-and-selectors/01-dom-manipulation/script.js
+++ fundamentals/section-5-dom-events-and-web-storage/day-2-dom-and-selectors/01-dom-manipulation/script.js
@@ -1,7 +1,7 @@
-const changeDescription = () => {
-  document.getElementsByTagName('p')[0].innerText = 'Me vejo daqui a 2 anos trabalhando com Desenvolvimento Web de maneira remota.';
-}
+const changeDescription = () => document.getElementsByTagName('p')[0].innerText = 'Me vejo daqui a 2 anos trabalhando com Desenvolvimento Web de maneira remota.';
+const changeYellowSquareColor = () => document.getElementsByClassName('main-content')[0].style["background-color"] = 'rgb(76,164,109)'
 
 
 
 changeDescription();
+changeYellowSquareColor();
