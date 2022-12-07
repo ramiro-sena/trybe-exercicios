02262884dbd483c2c55142cfa1ee94010b717e88
Ramiro <ramiro.sena.eng@gmail.com>
2022-12-07 16:20:50 -0300

    4th req accomplished - change text by tag name

--- fundamentals/section-5-dom-events-and-web-storage/day-2-dom-and-selectors/01-dom-manipulation/script.js
+++ fundamentals/section-5-dom-events-and-web-storage/day-2-dom-and-selectors/01-dom-manipulation/script.js
@@ -1,9 +1,13 @@
 const changeDescription = () => document.getElementsByTagName('p')[0].innerText = 'Me vejo daqui a 2 anos trabalhando com Desenvolvimento Web de maneira remota.';
 const changeYellowSquareColor = () => document.getElementsByClassName('main-content')[0].style["background-color"] = 'rgb(76,164,109)'
 const changeRedSquareColor = () => document.getElementsByClassName('center-content')[0].style["background-color"] = 'white'
-
+const changeTitle = () => {
+  let title = document.getElementsByTagName('h1')[0].innerText;
+  document.getElementsByTagName('h1')[0].innerHTML = title.toUpperCase();
+}
 
 
 changeDescription();
 changeYellowSquareColor();
 changeRedSquareColor();
+changeTitle();
