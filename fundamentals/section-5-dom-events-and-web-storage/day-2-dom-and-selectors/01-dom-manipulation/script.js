9d825780c7a7cba01a44d49382b8cccd747257a2
Ramiro <ramiro.sena.eng@gmail.com>
2022-12-07 16:15:31 -0300

    3rd req accopmlished - change color

--- fundamentals/section-5-dom-events-and-web-storage/day-2-dom-and-selectors/01-dom-manipulation/script.js
+++ fundamentals/section-5-dom-events-and-web-storage/day-2-dom-and-selectors/01-dom-manipulation/script.js
@@ -1,7 +1,9 @@
 const changeDescription = () => document.getElementsByTagName('p')[0].innerText = 'Me vejo daqui a 2 anos trabalhando com Desenvolvimento Web de maneira remota.';
 const changeYellowSquareColor = () => document.getElementsByClassName('main-content')[0].style["background-color"] = 'rgb(76,164,109)'
+const changeRedSquareColor = () => document.getElementsByClassName('center-content')[0].style["background-color"] = 'white'
 
 
 
 changeDescription();
 changeYellowSquareColor();
+changeRedSquareColor();
