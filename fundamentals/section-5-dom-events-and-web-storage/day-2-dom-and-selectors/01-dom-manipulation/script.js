e31ee2c90854141a67882468a5c6083b03fb985c
Ramiro <ramiro.sena.eng@gmail.com>
2022-12-07 16:07:33 -0300

    1st requirement accomplished - change tag text

---
+++ fundamentals/section-5-dom-events-and-web-storage/day-2-dom-and-selectors/01-dom-manipulation/script.js
@@ -0,0 +1,7 @@
+const changeDescription = () => {
+  document.getElementsByTagName('p')[0].innerText = 'Me vejo daqui a 2 anos trabalhando com Desenvolvimento Web de maneira remota.';
+}
+
+
+
+changeDescription();
