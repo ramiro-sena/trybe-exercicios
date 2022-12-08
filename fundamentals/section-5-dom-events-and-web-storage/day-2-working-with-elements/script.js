ad0d4a139469beb708abfbf6e26454754f15f0b0
Ramiro Sena <ramiro.sena.eng@gmail.com>
2022-12-08 19:23:51 -0300

    add main tag

--- fundamentals/section-5-dom-events-and-web-storage/day-2-working-with-elements/script.js
+++ fundamentals/section-5-dom-events-and-web-storage/day-2-working-with-elements/script.js
@@ -4,3 +4,8 @@ const bodyElement = document.getElementsByTagName('body')[0];
 const titleElement = document.createElement('h1')
 titleElement.innerText = 'TrybeTrip - Agência de Viagens';
 bodyElement.appendChild(titleElement);
+
+//02 Add main tag
+
+const main = document.createElement('main');
+bodyElement.appendChild(main);
