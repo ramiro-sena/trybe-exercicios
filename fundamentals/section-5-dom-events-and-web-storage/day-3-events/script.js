c846487d24783b29df7b3d485c2aa63a40a71811
Ramiro Sena <ramiro.sena.eng@gmail.com>
2022-12-09 23:42:54 -0300

    creating createDaysOfTheMonth function

--- fundamentals/section-5-dom-events-and-web-storage/day-3-events/script.js
+++ fundamentals/section-5-dom-events-and-web-storage/day-3-events/script.js
@@ -15,3 +15,15 @@ createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
 const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+const createDaysOfTheMonth = () => {
+  const ulElement = document.createElement('ul');
+  for (let day of decemberDaysList){
+    const liElement = document.createElement('li');
+    liElement.innerText = 'day';
+    liElement.className = 'day';
+    ulElement.appendChild(liElement);
+  }
+}
+
+createDaysOfTheMonth();
