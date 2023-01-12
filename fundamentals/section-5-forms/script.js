eae46ee951026d2d143708fd22484efa37323791
Ramiro Sena <ramiro.sena.eng@gmail.com>
2023-01-11 23:07:32 -0300

    create handleConsent function

--- fundamentals/section-5-forms/script.js
+++ fundamentals/section-5-forms/script.js
@@ -2,14 +2,18 @@ const form = document.querySelector('form');
 const submit = document.querySelector('#submit');
 const clear = document.querySelector('#clear');
 const fieldSet = document.querySelector('fieldset');
+const consent = document.querySelector('#consent');
 
-handleClear = () => {
+handleClear = (e) => {
   e.preventDefault();
 }
 
+handleConsent = (e) => {
+  submit.disabled = !e.target.checked
+}
 
 
 window.onload = () => {
   clear.addEventListener('click', handleClear);
-
+  consent.addEventListener('change', handleConsent)
 }
