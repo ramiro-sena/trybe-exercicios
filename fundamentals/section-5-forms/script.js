179621274aeb9e97bd1eec761c69c7afa1d3c577
Ramiro Sena <ramiro.sena.eng@gmail.com>
2023-01-11 23:16:39 -0300

    validate name

--- fundamentals/section-5-forms/script.js
+++ fundamentals/section-5-forms/script.js
@@ -12,8 +12,17 @@ handleConsent = (e) => {
   submit.disabled = !e.target.checked
 }
 
+validateForm = (e) => {
+  const name_len = document.querySelector('#full-name').value.length;
+  const val_name = name_len > 10 && name_len < 50;
+  
+  
+
+  console.log(val_name)
+}
 
 window.onload = () => {
   clear.addEventListener('click', handleClear);
-  consent.addEventListener('change', handleConsent)
+  consent.addEventListener('change', handleConsent);
+  submit.addEventListener('click', validateForm);
 }
