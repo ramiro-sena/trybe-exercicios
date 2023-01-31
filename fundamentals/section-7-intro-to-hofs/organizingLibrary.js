c72ef6861d18718196ad6e7ef736ee36c20b220f
Ramiro Sena <ramiro.sena.eng@gmail.com>
2023-01-30 22:41:03 -0300

    get named book

--- fundamentals/section-7-intro-to-hofs/organizingLibrary.js
+++ fundamentals/section-7-intro-to-hofs/organizingLibrary.js
@@ -15,7 +15,7 @@ const smallerName = (library) => {
     let nameBook;
     // escreva aqui o seu código
     library.forEach((book) => {
-        if (!nameBook || book.name.length < nameBook.length){
+        if (!nameBook || book.name.length < nameBook.length) {
             nameBook = book.name;
         }
     })
@@ -23,4 +23,22 @@ const smallerName = (library) => {
     return nameBook;
 }
 
-console.log(smallerName(books))
+// console.log(smallerName(books))
+
+
+//Encontre o primeiro livro cujo nome possua 26 caracteres.
+
+// const expectedResult = {
+//     author: {
+//       birthYear: 1948,
+//       name: 'George R. R. Martin',
+//     },
+//     genre: 'Fantasia',
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     releaseYear: 1991,
+//   };
+
+const getNamedBook = (books) =>  books.find((book) => book.name.length === 26);
+
+console.log(getNamedBook(books));
