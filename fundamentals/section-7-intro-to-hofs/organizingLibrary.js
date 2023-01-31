a7ce0fdfd06468b3908b21e94b5afa871f68d073
Ramiro Sena <ramiro.sena.eng@gmail.com>
2023-01-30 22:37:11 -0300

    find book with shortest name

--- fundamentals/section-7-intro-to-hofs/organizingLibrary.js
+++ fundamentals/section-7-intro-to-hofs/organizingLibrary.js
@@ -2,10 +2,25 @@ const books = require('./books.js');
 
 //Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
 
-const expectedResult = 'Stephen King';
+//const expectedResult = 'Stephen King';
 const authorBornIn1947 = () => {
     const book = books.find((book) => book.author.birthYear === 1947);
     return book.author.name;
 };
 
-console.log(authorBornIn1947());
+//Retorne o nome do livro com menor número de caracteres (menor nome).
+
+//const expectedResult = 'Duna';
+const smallerName = (library) => {
+    let nameBook = library[0].name;
+    // escreva aqui o seu código
+    library.forEach((book) => {
+        if (book.name.length < nameBook.length){
+            nameBook = book.name;
+        }
+    })
+    // Variável nameBook que receberá o valor do menor nome;
+    return nameBook;
+}
+
+console.log(smallerName(books))
