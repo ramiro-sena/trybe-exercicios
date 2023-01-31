7230279e0d0263131792ca5ccfab904d9aad398f
Ramiro Sena <ramiro.sena.eng@gmail.com>
2023-01-30 22:49:24 -0300

    create someBookWasReleaseOnThe80s function

--- fundamentals/section-7-intro-to-hofs/organizingLibrary.js
+++ fundamentals/section-7-intro-to-hofs/organizingLibrary.js
@@ -52,4 +52,15 @@ function everyoneWasBornOnSecXX(books) {
   return books.every((book) => book.author.birthYear <= 2000 && book.author.birthYear >= 1900);
 };
 
-console.log(everyoneWasBornOnSecXX(books))
+// console.log(everyoneWasBornOnSecXX(books))
+
+//Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
+
+//const expectedResult = true;
+
+const someBookWasReleaseOnThe80s = (books) => {
+  // escreva seu código aqui
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);  
+}
+
+// console.log(someBookWasReleaseOnThe80s(books))
