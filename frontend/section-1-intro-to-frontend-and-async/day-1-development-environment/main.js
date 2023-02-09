f3208e4b960045d4a409cb314950b3cb4d132df8
Ramiro Sena <ramiro.sena.eng@gmail.com>
2023-02-09 10:11:18 -0300

    create fieldSet object

--- frontend/section-1-intro-to-frontend-and-async/day-1-development-environment/main.js
+++ frontend/section-1-intro-to-frontend-and-async/day-1-development-environment/main.js
@@ -1,1 +1,22 @@
-import "./style.css"
+import "./style.css";
+import validator from "validator";
+
+const input = document.querySelector("input");
+const dropdown = document.querySelector("select");
+const buttonVal = document.querySelector("button");
+const label = document.querySelector("#output-label");
+
+const fieldSet = [
+  { field: "CPF", validate: (value) => validator.isVAT(value, "BR") },
+  { field: "Email", validate: (value) => validator.isEmail(value) },
+  {
+    field: "Phone",
+    validate: (value) => validator.isMobilePhone(value, "pt-BR"),
+  },
+  { field: "CEP", validate: (value) => validator.isVAT(value, "BR") },
+  {
+    field: "Date",
+    validate: (value) => validator.isDate(value, { format: "DD/MM/YYYY" }),
+  },
+];
+
