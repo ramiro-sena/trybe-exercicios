508e8ffc0071306f29bf00056cf6cbd78620a8d6
Ramiro Sena <ramiro.sena.eng@gmail.com>
2023-02-13 13:13:54 -0300

    add dropdown logic

--- frontend/section-1-intro-to-frontend-and-async/day-1-development-environment/main.js
+++ frontend/section-1-intro-to-frontend-and-async/day-1-development-environment/main.js
@@ -7,16 +7,29 @@ const buttonVal = document.querySelector("button");
 const label = document.querySelector("#output-label");
 
 const fieldSet = [
-  { field: "CPF", validate: (value) => validator.isVAT(value, "BR") },
-  { field: "Email", validate: (value) => validator.isEmail(value) },
+  { name: "CPF", validate: (value) => validator.isVAT(value, "BR") },
+  { name: "Email", validate: (value) => validator.isEmail(value) },
   {
-    field: "Phone",
+    name: "Phone",
     validate: (value) => validator.isMobilePhone(value, "pt-BR"),
   },
-  { field: "CEP", validate: (value) => validator.isVAT(value, "BR") },
+  { name: "CEP", validate: (value) => validator.isPostalCode(value, "BR") },
   {
-    field: "Date",
+    name: "Date",
     validate: (value) => validator.isDate(value, { format: "DD/MM/YYYY" }),
   },
 ];
 
+window.onload = () => {
+  fieldSet.forEach((field) => {
+    const option = document.createElement("option");
+    option.innerText = field.name;
+    dropdown.appendChild(option);
+  });
+
+  buttonVal.addEventListener("click", (event) => {
+    event.preventDefault();
+    const field = fieldSet.find((field) => field.name === dropdown.value);
+    label.innerHTML = field.validate(input.value)
+  });
+};
